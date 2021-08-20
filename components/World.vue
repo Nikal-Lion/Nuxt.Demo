3f3237ea1d6d57cfd1eeb6bbab9e4bd5a3120fe8
wolfife <wolfife@163.com>
2021-08-20 14:55:53 +0800

    nuxt demo initial a demo project of nuxt

---
+++ components/World.vue
@@ -0,0 +1,32 @@
+<template lang="">
+  <div>
+    <h1>
+      Test {{ name }}
+    </h1>
+  </div>
+</template>
+<script>
+  export default {
+    name: "World",
+    props: {
+      id: {
+        type: Number,
+        default: 0
+      }
+    },
+    computed: {
+      name() {
+          return this.id + ' hello'
+      },
+    },
+    data() {
+      return {
+
+      }
+    },
+  }
+
+</script>
+<style lang="">
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <!-- <Tutorial/> -->
+  <component :is="componentId" :id="90"></component>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data() {
+    return {
+      componentId:"World"
+    }
+  },
+  methods: {
+    
+  },
+})
+</script>
